8c2933aac24f0b9793e2fc2103d6faff514f4bbc
IvanditoRakaputra <114067618+IvanditoRakaputra@users.noreply.github.com>
2022-12-20 23:25:32 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //Please put your answer here
+$(function() {
+  $('.signup-show').click(function() {
+    $('#signup-modal').fadeIn();
+  });
+});
+  $('.fa-times').click(function() {
+    $('#signup-modal').fadeOut();
+  });
+});
